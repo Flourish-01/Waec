ee88dbd24f4a362359f86f6b23e2b012cc2dd4eb
starr <flourishamao@gmail.com>
2023-12-10 22:31:56 +0100

    waec

--- m.js
+++
@@ -1,8 +0,0 @@
-intro.innerHTML = "Welcome " + document.getElementById('studentName').value
-note.innerHTML = "It is my utmost pleasure to sincerely appreciate your efforts for this semester. Please, find below how you have performed. Best regards👋."
-
-if(cumulative >= 0 && cumulative <40) {
-    result.innerHTML = "Your score is " + cumulative + "%. Unfortunately, you will not be progressing to the next level. I will suggest you reconsider your intent to study in our noble institution. Thank you and best wishes."
-} else if (cumulative >= 40 && cumulative < 60) {
-    result.innerHTML = "Your score is " + cumulative + "%. Well done, however, this is not good enough. Please try harder next year, " + studentName.value
-}
